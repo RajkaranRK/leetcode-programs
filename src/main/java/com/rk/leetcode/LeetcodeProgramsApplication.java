4d7ef406b68dca2ce65de05d084091f4b59e5562
RajkaranRK <rajkaranrk164@gmail.com>
2022-07-18 16:02:47 +0530

    code added

---
+++ src/main/java/com/rk/leetcode/LeetcodeProgramsApplication.java
@@ -0,0 +1,13 @@
+package com.rk.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeProgramsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeProgramsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rk/leetcode/LeetcodeProgramsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rk.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeProgramsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
