fd9bca84cbf74f347772799f09b72a6c843adbba
RajkaranRK <rajkaranrk164@gmail.com>
2022-07-19 17:27:35 +0530

    code added for longest palindrom string

---
+++ src/main/java/com/rk/leetcode/interview/LongestPalindrom.java
@@ -0,0 +1,47 @@
+package com.rk.leetcode.interview;
+
+import com.sun.deploy.util.StringUtils;
+
+public class LongestPalindrom {
+
+    public static void main(String []args){
+        LongestPalindrom palindrom = new LongestPalindrom();
+        String palindromStr = palindrom.longestPalindrome("babad");
+        System.out.println("Palindrom string "+palindromStr);
+
+        palindromStr = palindrom.longestPalindrome("ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc");
+        System.out.println("Palindrom string "+palindromStr);
+    }
+
+    public String longestPalindrome(String s) {
+        String maxStr = "";
+        int i =0;
+        while(i<s.length()){
+            char startCh = s.charAt(i);
+            for(int j= s.length()-1; j >= i; j--){
+                if(startCh == s.charAt(j)){
+                    String str = s.substring(i,j+1);
+                    if(isPalindrom(str) && str.length() > maxStr.length()){
+                        maxStr = str;
+                    }
+                }
+            }
+            if(maxStr.length() == s.length()){
+                break;
+            }
+            i++;
+        }
+        return maxStr;
+    }
+
+    private boolean isPalindrom(String str){
+        boolean isPalindrom = true;
+        for(int i=0,j=str.length()-1; i<str.length()/2;i++,j--){
+            if(str.charAt(i) != str.charAt(j)){
+                isPalindrom = false;
+                break;
+            }
+        }
+        return isPalindrom;
+    }
+}
