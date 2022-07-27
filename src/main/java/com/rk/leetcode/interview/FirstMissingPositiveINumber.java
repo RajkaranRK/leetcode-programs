5307652ce28b6f97053fdb10e82eb314fdb4a54b
Raj Karan <raj.karan@africa.airtel.com>
2022-07-27 19:22:38 +0530

    First missing positive integer code added

---
+++ src/main/java/com/rk/leetcode/interview/FirstMissingPositiveINumber.java
@@ -0,0 +1,34 @@
+package com.rk.leetcode.interview;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class FirstMissingPositiveINumber {
+    
+    public static void main(String []args) {
+        int[] arr = new int[] {3,4,-1,1};
+        FirstMissingPositiveINumber missingPositiveINumber = new FirstMissingPositiveINumber();
+        int num = missingPositiveINumber.firstMissingPositive(arr);
+        System.out.print(num);
+    }
+    
+    
+    public int firstMissingPositive(int[] nums) {
+        Set<Integer> numSet = new TreeSet<>();
+        for(int i=0; i<nums.length;i++) {
+            if(nums[i]>0)
+                numSet.add(nums[i]);
+        }
+        Iterator<Integer> iter = numSet.iterator();
+        int count = 0;
+        while(iter.hasNext()) {
+            if(!iter.next().equals(count+1)) {
+                return count+1;
+            }
+            count++;
+        }
+        return count+1;
+    }
+
+}
