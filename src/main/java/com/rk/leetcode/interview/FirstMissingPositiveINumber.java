47c000cf21fa280220cb37db66c7d3aa78b4f85a
Raj Karan <raj.karan@africa.airtel.com>
2022-07-27 20:02:59 +0530

    Added another solution

--- src/main/java/com/rk/leetcode/interview/FirstMissingPositiveINumber.java
+++ src/main/java/com/rk/leetcode/interview/FirstMissingPositiveINumber.java
@@ -11,6 +11,9 @@ public class FirstMissingPositiveINumber {
         FirstMissingPositiveINumber missingPositiveINumber = new FirstMissingPositiveINumber();
         int num = missingPositiveINumber.firstMissingPositive(arr);
         System.out.print(num);
+        
+        num = missingPositiveINumber.firstMissingPositive2(arr);
+        System.out.print(num);
     }
     
     
@@ -29,6 +32,33 @@ public class FirstMissingPositiveINumber {
             count++;
         }
         return count+1;
+    }
+    
+    
+    public int firstMissingPositive2(int nums[]) {
+        //mark the element
+        boolean one = false;
+        for(int i=0; i<nums.length;i++) {
+            if(nums[i] == 1) {
+                one = true;
+            }
+            if(nums[i] < 1 || nums[i] > nums.length) {
+                nums[i] = 1;
+            }
+        }
+        if(!one) {
+            return 1;
+        }
+        for(int i=0;i<nums.length;i++) {
+            int index = Math.abs(nums[i]);
+            nums[index-1] = -Math.abs(nums[index-1]); 
+        }
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] > 0) {
+                return i+1;
+            }
+        }
+        return nums.length+1;
     }
 
 }
