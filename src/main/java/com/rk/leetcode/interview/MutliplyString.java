c6823769737593e9ff0829e4c6a5704073256f2f
Raj Karan <raj.karan@africa.airtel.com>
2022-07-27 17:56:52 +0530

    code added for multiply string

--- src/main/java/com/rk/leetcode/interview/MutliplyString.java
+++ src/main/java/com/rk/leetcode/interview/MutliplyString.java
@@ -6,61 +6,43 @@ public class MutliplyString {
     
     public static void main(String []args) {
         MutliplyString mulMutliplyString = new MutliplyString();
-        System.out.print(mulMutliplyString.multiply("122", "12"));
+        String result = mulMutliplyString.multiply("498828660196", "840477629533");
+        System.out.print(result);
     }
+   
     
     public String multiply(String num1, String num2) {
         if(num1.equals("0") || num2.equals("0"))
             return "0";
-        String result = "";
-        if(num1.length() >= num2.length()){
-            result = perform(num1,num2);
-        }else{
-            result = perform(num2,num1);
+        if(num1.equals("1")) {
+            return num2;
         }
+        if(num2.equals("1")) {
+            return num1;
+        }
+        int m = num1.length();
+        int n = num2.length();
+        int arr[] = new int[m+n];
         
-        return result;
-    }
-    
-    
-    public String perform(String str1, String str2){
-        String sum = "";
-        for(int i=0; i< str2.length() ;i++) {
-            sum = "";
-            int num2 = str2.charAt(i) - '0';
-            int carry = 0;
-            String result = "";
-            for(int j=i; j<str1.length();j++) {
-                int num1 = str1.charAt(j) - '0';
-                int multiply = num1 * num2 + carry;
-                result =  result+multiply;
-                if(sum.equals("")) {
-                    sum = result;
-                }else {
-                    sum = addString(sum,result, i);
-                }
+        for(int i= m-1; i>=0;i--) {
+            for(int j = n - 1; j>=0; j--) {
+                int product = (num1.charAt(i)- '0') * (num2.charAt(j)-'0');
+                product = product + arr[i+j+1];
+                arr[i+j+1] = product % 10;
+                arr[i+j] = arr[i+j]+product / 10;
             }
         }
-        return sum;
+        
+        StringBuilder builer = new StringBuilder();
+        boolean flag = true;
+        for(int i=0;i<arr.length;i++) {
+            if(flag && arr[i] == 0){
+                continue;
+            }
+            flag = false;
+            builer.append(arr[i]);
+        }
+        return builer.toString();
     }
     
-    public String addString(String str1, String str2,int index) {
-        int counter = 0 ;
-        int carry = 0 ;
-        String sumStr = "";
-        for(int i= str2.length()-1; i>=0; i--) {
-            int num1 = str2.charAt(i) - '0';
-            int num2 = 0;
-            if(str1.length()-1 == i) {
-                num2 = str1.charAt(i) - '0';
-            }
-            int sum = num1 + num2 + carry;
-            carry = carry / 10;
-            sum = sum % 10;
-            counter++;
-            sumStr = sumStr + sum;
-        }
-        return sumStr;
-        
-    }
 }
