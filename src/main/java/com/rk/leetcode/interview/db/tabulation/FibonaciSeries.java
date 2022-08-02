ab9d757e5db946935472a33c11786896e9a3d149
Raj Karan <raj.karan@africa.airtel.com>
2022-08-02 10:50:45 +0530

    code added for fibonaci series using tab

---
+++ src/main/java/com/rk/leetcode/interview/db/tabulation/FibonaciSeries.java
@@ -0,0 +1,36 @@
+package com.rk.leetcode.interview.db.tabulation;
+
+
+/**
+ * Dynamic programming approach using Tabulation
+ * @author RajKaran
+ *
+ */
+public class FibonaciSeries {
+    
+    public static void main(String []args) {
+        FibonaciSeries fibonaciSeries = new FibonaciSeries();
+        System.out.println(fibonaciSeries.fib(6));
+        
+    }
+    
+    
+    public int fib(int n) {
+        if(n <=0) {
+            return n;
+        }
+        int fib[] = new int[n+1];
+        fib[0] = 0;
+        fib[1] = 1;
+        for(int i=1;i<=n;i++) {
+            if(i+1 <= n) {
+                fib[i+1] = fib[i+1] + fib[i];
+            }
+            if(i+2 <= n) {
+                fib[i+2] = fib[i+2] + fib[i];
+            }
+        }
+        return fib[n];
+    }
+
+}
