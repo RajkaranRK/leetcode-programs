b89d0654abbb9ab88691844db8b3a66711c08562
RajkaranRK <rajkaranrk164@gmail.com>
2022-08-03 22:16:45 +0530

    code not added for gridtraveler

--- src/main/java/com/rk/leetcode/interview/db/tabulation/GridTraveler.java
+++ src/main/java/com/rk/leetcode/interview/db/tabulation/GridTraveler.java
@@ -1,4 +1,14 @@
 package com.rk.leetcode.interview.db.tabulation;
 
 public class GridTraveler {
+
+    public static void main(String []args){
+        GridTraveler gridTraveler = new GridTraveler();
+        gridTraveler.findAllPossiblePath(3,2);
+    }
+
+
+    public long findAllPossiblePath(int m,int n){
+
+    }
 }
