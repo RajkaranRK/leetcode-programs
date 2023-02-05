92bc5deabb36fb8fa4f4727ad4df5c1d63803eb8
RajkaranRK <rajkaranrk164@gmail.com>
2023-02-05 11:41:48 +0530

    code added for Longestsubstring return

--- src/main/java/com/rk/leetcode/interview/LongestSubString.java
+++ src/main/java/com/rk/leetcode/interview/LongestSubString.java
@@ -1,6 +1,7 @@
 package com.rk.leetcode.interview;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class LongestSubString {
@@ -11,7 +12,9 @@ public class LongestSubString {
         LongestSubString subString = new LongestSubString();
         int length = subString.getLongestSubStringLength(s);
         System.out.println("Longest sub string length "+length);
-        
+
+        String str = subString.getLongestSubString(s);
+        System.out.println("Longest subs-string "+str);
     }
     
     
@@ -33,6 +36,30 @@ public class LongestSubString {
             }
         }
         return max;
+    }
+
+
+    public String getLongestSubString(String s){
+        String maxStr = "";
+        int maxLen = 0,i=0,j=0;
+        Set<Character> charSet = new LinkedHashSet<>();
+        char []arr = s.toCharArray();
+        while( i < arr.length){
+            if(!charSet.contains(arr[i])){
+                charSet.add(arr[i]);
+                if(maxLen < charSet.size()){
+                    maxLen = charSet.size();
+                    StringBuilder builder = new StringBuilder();
+                    charSet.stream().forEach(ch -> builder.append(ch));
+                    maxStr = builder.toString();
+                }
+                i++;
+            }else{
+                charSet.remove(arr[j++]);
+            }
+        }
+        System.out.println("Max Length "+maxLen);
+        return maxStr;
     }
     
     
