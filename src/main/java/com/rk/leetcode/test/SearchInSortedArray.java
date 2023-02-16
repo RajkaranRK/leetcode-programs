d6975494a848cea0ab8b7f521165dc9025c5d69c
Raj Karan <raj.karan@africa.airtel.com>
2023-02-16 18:58:06 +0530

    code updated for search in sroted rotated array

--- src/main/java/com/rk/leetcode/test/SearchInSortedArray.java
+++ src/main/java/com/rk/leetcode/test/SearchInSortedArray.java
@@ -3,7 +3,10 @@ package com.rk.leetcode.test;
 public class SearchInSortedArray {
 
     public static void main(String []args){
-
+        int arr[] = new int[] {2,4,5,6,7,0,1};
+        int key = 0;
+        int index = binarySearchSortedArray(arr, key);
+        System.out.println("Found " + index);
     }
 
     public static int binarySearchSortedArray(int arr[],int key){
@@ -11,17 +14,25 @@ public class SearchInSortedArray {
         int end = arr.length - 1;
         while(start <= end){
             int mid = (start+end)/2;
-            if(arr[mid] == arr[key]){
+            if(arr[mid] == key){
                 return mid;
             }
             //check is left sorted
-            if(arr[mid] >= arr[start] || arr){
-
+            if(arr[mid] > arr[start]){
+                if(key >= arr[start] && key < arr[mid]) {
+                    end = mid - 1;
+                }else {
+                    start = mid + 1;
+                }
             }else{
                 //else right sorted
-
+                if(key > arr[mid] && key <= arr[end]) {
+                    start = mid + 1;
+                }else {
+                    end = mid - 1;
+                }
             }
         }
-
+        return -1;
     }
 }
