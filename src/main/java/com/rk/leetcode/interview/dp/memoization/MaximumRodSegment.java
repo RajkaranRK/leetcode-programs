4eaebcc199c806e15987a50baef77ac345b92653
RajkaranRK <rajkaranrk164@gmail.com>
2023-02-18 14:26:35 +0530

    code added for maximum rod sagment

--- src/main/java/com/rk/leetcode/interview/dp/memoization/MaximumRodSegment.java
+++ src/main/java/com/rk/leetcode/interview/dp/memoization/MaximumRodSegment.java
@@ -1,15 +1,21 @@
 package com.rk.leetcode.interview.dp.memoization;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MaximumRodSegment {
 
     public static void main(String[] args) {
-        int sum = 7;
-        int arr[] = new int[] {3,6,3};
-        int maxCount = solve(arr,sum);
+        int sum = 10000;
+        int arr[] = new int[] {3,6,1};
+        int maxCount = solve(arr,sum,new HashMap<>());
         System.out.println(maxCount == Integer.MIN_VALUE ? -1 : maxCount);
     }
 
-    private static int solve(int arr[],int sum){
+    private static int solve(int arr[], int sum, Map<Integer,Integer> memo){
+        if(memo.containsKey(sum)){
+            return memo.get(sum);
+        }
         if(sum == 0){
             return 0;
         }
@@ -18,7 +24,7 @@ public class MaximumRodSegment {
         }
         int max = Integer.MIN_VALUE;
         for(int i =0; i<arr.length; i++){
-            int temp = solve(arr,sum - arr[i]);
+            int temp = solve(arr,sum - arr[i],memo);
             if(temp != Integer.MIN_VALUE){
                 temp = temp + 1;
                 if(temp > max){
@@ -26,6 +32,7 @@ public class MaximumRodSegment {
                 }
             }
         }
+        memo.put(sum,max);
         return max;
     }
 }
