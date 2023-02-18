10ce4ccaab1a956fa55d36884bc572ae83785f8e
RajkaranRK <rajkaranrk164@gmail.com>
2023-02-18 08:51:09 +0530

    code added for stair possible count

--- src/main/java/com/rk/leetcode/interview/dp/memoization/StairPossibleWay.java
+++ src/main/java/com/rk/leetcode/interview/dp/memoization/StairPossibleWay.java
@@ -1,2 +1,29 @@
-package com.rk.leetcode.interview.dp.memoization;public class StairPossibleWay {
+package com.rk.leetcode.interview.dp.memoization;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StairPossibleWay {
+
+    public static void main(String[] args) {
+        int count = climbStair(1000,new HashMap<>());
+        System.out.println(count);
+    }
+
+
+    private static int climbStair(int n, Map<Integer,Integer> memo){
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+        if(n==0){
+            return 1;
+        }
+        if(n < 0){
+            return 0;
+        }
+        int count = climbStair(n-1,memo) + climbStair(n-2,memo);
+        memo.put(n,count);
+        return count;
+    }
 }
