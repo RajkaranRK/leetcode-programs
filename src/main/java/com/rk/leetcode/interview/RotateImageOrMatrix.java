aac73dbe0828777dc397d23f02e2f703d89167b8
Raj Karan <raj.karan@africa.airtel.com>
2023-03-09 21:16:04 +0530

    class added Rotateimage

---
+++ src/main/java/com/rk/leetcode/interview/RotateImageOrMatrix.java
@@ -0,0 +1,20 @@
+package com.rk.leetcode.interview;
+
+public class RotateImageOrMatrix {
+    
+    public static void main(String []args) {
+        int [][]matrix = new int[][] {{1,2,3},{4,5,6},{7,8,9}};
+        
+        //Step 1 Transpose the matrix
+        
+        //Step 2 reverse each row
+        
+
+    }
+    
+    
+    private void transpose(int [][]matrix) {
+        
+    }
+
+}
