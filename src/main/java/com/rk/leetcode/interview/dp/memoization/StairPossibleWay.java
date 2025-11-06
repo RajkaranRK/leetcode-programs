7142daa14085b74f3bac8e68fc5e1a69724db06d
rajkaranrk164@gmail.com <rajkaranrk164@gmail.com>
2025-11-06 22:22:02 +0530

    stairs problem solution

--- src/main/java/com/rk/leetcode/interview/dp/memoization/StairPossibleWay.java
+++ src/main/java/com/rk/leetcode/interview/dp/memoization/StairPossibleWay.java
@@ -1,29 +1,44 @@
 package com.rk.leetcode.interview.dp.memoization;
 
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 public class StairPossibleWay {
 
     public static void main(String[] args) {
-        int count = climbStair(1000,new HashMap<>());
-        System.out.println(count);
+        int n = 1000;
+        log.info("Climb stairs : {}",climbStairs(n,new HashMap<>()));
+        log.info("Climb Stairs : {}",climbStairs(n));
     }
 
-
-    private static int climbStair(int n, Map<Integer,Integer> memo){
+    public static int climbStairs(int n, Map<Integer,Integer> memo){
         if(memo.containsKey(n)){
             return memo.get(n);
         }
-        if(n==0){
+        if( n == 0){
             return 1;
         }
-        if(n < 0){
-            return 0;
+        if(n == 1){
+            return 1;
         }
-        int count = climbStair(n-1,memo) + climbStair(n-2,memo);
-        memo.put(n,count);
-        return count;
+        int result =  climbStairs(n-1,memo) + climbStairs(n-2,memo);
+        memo.put(n,result);
+        return result;
+    }
+
+    public static int climbStairs(int n){
+        int memo[] = new int[n+1];
+        Arrays.fill(memo,-1);
+        memo[0] = 1;
+        memo[1] = 1;
+        for (int i=2; i<=n; i++){
+            memo[i] = memo[i-1] + memo[i-2];
+        }
+        return memo[n];
     }
 }
