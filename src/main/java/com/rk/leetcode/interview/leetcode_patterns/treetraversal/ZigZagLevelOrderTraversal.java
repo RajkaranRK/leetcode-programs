f1086501feefa4c4f4646c1e7c3e287271629492
rajkaranrk164@gmail.com <rajkaranrk164@gmail.com>
2025-11-12 11:13:50 +0530

    zig zag traversal

---
+++ src/main/java/com/rk/leetcode/interview/leetcode_patterns/treetraversal/ZigZagLevelOrderTraversal.java
@@ -0,0 +1,58 @@
+package com.rk.leetcode.interview.leetcode_patterns.treetraversal;
+
+import com.rk.leetcode.TreeNode;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+
+/**
+ * Given the root of a binary tree, return the zigzag level order traversal of its nodes’ values.
+ * (i.e. from left to right, then right to left for the next level, and so on).
+ */
+
+@Slf4j
+public class ZigZagLevelOrderTraversal {
+
+    public static void main(String[] args) {
+        TreeNode root = TreeBuilder.buildTree(new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15});
+        zigZagTraverse(root);
+        System.out.println("");
+        log.info("******************");
+    }
+
+    public static void zigZagTraverse(TreeNode root){
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        List<Integer> results = new ArrayList<>();
+        boolean invertFlag = false;
+        while(!queue.isEmpty()) {
+            List<TreeNode> nodes = new ArrayList<>();
+            while (!queue.isEmpty()) nodes.add(queue.poll());
+            log.info("Populated nodes {} ", nodes.stream().map(node->node.val).toList());
+            if (invertFlag && nodes.size() > 0) {
+                //start from end and first insert the right then
+                int i = nodes.size() - 1;
+                while(i >=0){
+                    results.add(nodes.get(i).val);
+                    TreeNode node = nodes.get(nodes.size() - 1 - i);
+                    if(node.left != null) queue.offer(node.left);
+                    if (node.right != null) queue.offer(node.right);
+                    i--;
+                }
+            }else{
+                for (int i=0; i< nodes.size(); i++){
+                    TreeNode node = nodes.get(i);
+                    results.add(node.val);
+                    if(node.left != null) queue.offer(node.left);
+                    if(node.right != null) queue.offer(node.right);
+                }
+            }
+            invertFlag = !invertFlag;
+        }
+        log.info("Traversal Result : {}",results);
+    }
+}
