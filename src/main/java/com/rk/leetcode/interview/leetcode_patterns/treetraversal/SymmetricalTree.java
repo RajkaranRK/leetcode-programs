90d3b81c8c80fba33f74a42307ee80e054553c36
rajkaranrk164@gmail.com <rajkaranrk164@gmail.com>
2025-11-13 11:41:33 +0530

    code for symmetrical added

---
+++ src/main/java/com/rk/leetcode/interview/leetcode_patterns/treetraversal/SymmetricalTree.java
@@ -0,0 +1,21 @@
+package com.rk.leetcode.interview.leetcode_patterns.treetraversal;
+
+import com.rk.leetcode.TreeNode;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+public class SymmetricalTree {
+
+    public static void main(String[] args) {
+        TreeNode root = TreeBuilder.buildTreeV2(new Integer[]{1,2,2,3,4,4,3});
+        log.info("Is tree symmetric :{} ",root == null || checkSymmetry(root.left, root.right));
+    }
+
+    public static boolean checkSymmetry(TreeNode t1, TreeNode t2){
+        if(t1 == null || t2 == null){
+            return t1 == t2;
+        }
+        return t1.val == t2.val && checkSymmetry(t1.left, t2.right) && checkSymmetry(t1.right,t2.left);
+    }
+}
