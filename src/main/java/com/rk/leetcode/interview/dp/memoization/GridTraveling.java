e1da9ec56ef527cf87a77114d7dcc538b3cd29a1
rajkaranrk164@gmail.com <rajkaranrk164@gmail.com>
2025-11-20 22:20:21 +0530

    grid travel in 3 direction

--- src/main/java/com/rk/leetcode/interview/dp/memoization/GridTraveling.java
+++ src/main/java/com/rk/leetcode/interview/dp/memoization/GridTraveling.java
@@ -1,4 +1,36 @@
 package com.rk.leetcode.interview.dp.memoization;
 
+import lombok.extern.slf4j.Slf4j;
+
+
+
+@Slf4j
 public class GridTraveling {
+
+    public static void main(String[] args) {
+        int [][]costs = {
+                {1, 2, 10, 4},
+                {100, 3, 2, 1},
+                {1, 1, 20, 2},
+                {1, 2, 2, 1}
+        };
+        int max = Integer.MIN_VALUE;
+        for (int i =0; i < costs[0].length; i++){
+            max = Math.max(max,maximumPath3Direction(costs,0,i));
+        }
+        log.info("Maximum costs for traveling in 3 direction : {}",max);
+    }
+
+    public static int maximumPath3Direction(int [][]costs, int i, int j){
+        if(i >= costs.length || j >= costs[0].length || i < 0 || j < 0){
+            return Integer.MIN_VALUE;
+        }
+        if(i == costs.length - 1){
+            return costs[i][j];
+        }
+        int first = maximumPath3Direction(costs,i+1,j);
+        int second = maximumPath3Direction(costs,i+1, j+1);
+        int third = maximumPath3Direction(costs,i+1, j-1);
+        return costs[i][j] + Math.max(Math.max(first,second),third);
+    }
 }
