5d002168eae2612daa026d4535781d45f7372c65
rajkaranrk164@gmail.com <rajkaranrk164@gmail.com>
2025-11-21 11:41:33 +0530

    all prefix sum code added

--- src/main/java/com/rk/leetcode/interview/prefixsum/CanPrefixSum.java
+++ src/main/java/com/rk/leetcode/interview/prefixsum/CanPrefixSum.java
@@ -10,15 +10,17 @@ import java.util.*;
 public class CanPrefixSum {
 
     public static void main(String[] args) {
-        int[] arr = {9,4,0,20,3,10,5};
-        int target = 9;
+        int []arr = {1,2,2,3,4,2,5,6,7};
+        int target = 7;
 
-        log.info("prefix sum exists {} ",canPrefixSum(arr,target));
+        log.info("prefix sum exists: {} ",canPrefixSum(arr,target));
 
-        log.info("prefix sum first {}",howPrefixSum(arr,target));
+        log.info("prefix sum first: {}",howPrefixSum(arr,target));
 
 
-        log.info("prefix sum count {}",countPrefixSum(arr,target));
+        log.info("prefix sum count: {}",countPrefixSum(arr,target));
+
+        log.info("all prefix sum: {}",allPrefix(arr,target));
 
 
     }
@@ -46,7 +48,6 @@ public class CanPrefixSum {
         for(int i=0; i < arr.length; i++){
             prefixSum += arr[i];
             if(dataMap.containsKey(prefixSum - target)){
-                log.info("key :{}",prefixSum-target);
                 return Arrays.asList(dataMap.get(prefixSum-target)+1,i);
             }
             if(!dataMap.containsKey(prefixSum)){
@@ -75,4 +76,27 @@ public class CanPrefixSum {
         }
         return  count;
     }
+
+
+    public static List<List<Integer>> allPrefix(int []arr,int k){
+        Map<Integer,List<Integer>> prefixSumMap = new HashMap<>();
+        int prefixSum = 0;
+        List<Integer> list = new ArrayList<>();
+        list.add(-1);
+        prefixSumMap.put(0,list);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i =0; i<arr.length; i++){
+            prefixSum = prefixSum + arr[i];
+            if(prefixSumMap.containsKey(prefixSum - k)){
+                for (int index : prefixSumMap.get(prefixSum-k)){
+                    result.add(List.of(index + 1,i));
+                }
+            }
+            List<Integer> indexes = prefixSumMap.getOrDefault(prefixSum,new ArrayList<>());
+            indexes.add(i);
+            prefixSumMap.put(prefixSum,indexes);
+        }
+        return result;
+    }
+
 }
